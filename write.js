d686c033223aebf899be5089ab68ce6f7f3dd16d
Void-Architect1 <vuson6502@gmail.com>
2026-09-21 22:54:30 +0700

    Update write.js

--- write.js
+++ write.js
@@ -286,7 +286,15 @@ window.toggleLoboFold = function(headerElement) {
         foldContainer.classList.add('open');
         contentDiv.style.maxHeight = contentDiv.scrollHeight + 'px';
 
-		headerElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+		setTimeout(() => {
+            const elementPosition = headerElement.getBoundingClientRect().top;
+            const offsetPosition = elementPosition + window.pageYOffset - 20;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        }, 120);
         const videoSrc = foldContainer.dataset.video;
         const musicSrc = foldContainer.dataset.music;
         const videoEl = foldContainer.querySelector('video');
